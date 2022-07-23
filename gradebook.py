debbb4bc120f127809423a36d72a0c703a39556c
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-07-23 14:26:23 -0400

    added instructor class

--- gradebook.py
+++ gradebook.py
@@ -1,35 +1,46 @@
 from enum import Enum
+from uuid import uuid4
 
 class AliveStatus(Enum):
     DECEASED = 0
     ALIVE = 1
 
 class Person():
-    def __init__(self,first_name, last_name, dob):
+    def __init__(self,first_name, last_name, dob, AliveStatus):
         self.first_name = first_name
         self.last_name = last_name
         self.dob = dob
+        self.alive = AliveStatus
 
     def update_first_name(self, first_name):
         self.first_name = first_name
+        return first_name
 
     def update_last_name(self, last_name):
         self.last_name = last_name
+        return last_name
 
     def update_dob(self, dob):
         self.dob = dob
+        return dob
 
-    def update_status(self, alive):
-        self.alive = alive
+    def update_status(self, status):
+        self.alive = status
 
 
 class Instructor(Person):
+    def __init__(self, first_name, last_name, dob):
+        super().__init__(first_name,last_name,dob)
+
+        self.instructor_id = (f'Instructor_{uuid4()}')
+
+
 # This class must inherit from the Person class.
 # This class must have an additional attribute called instructor_id.
 # The instructor_id attribute must start with the string "Instructor_" followed by a UUID value.
 # Hint: Use super class calls.
 
-pass
+
 
 
 class Student(Person):
